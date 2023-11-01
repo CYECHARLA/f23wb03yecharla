2c08bd3fa883fc0564bd614b659668bb973f58c6
CYECHARLA <142927564+CYECHARLA@users.noreply.github.com>
2023-11-01 11:04:08 -0500

    correct the serach results

--- routes/cars.js
+++ routes/cars.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const results = [
+  {
+    make: 'Toyota',
+    model: 'Camry',
+    price: 25000
+  },
+  {
+    make: 'Honda',
+    model: 'Civic',
+    price: 22000
+  },
+  {
+    make: 'Ford',
+    model: 'F-150',
+    price: 35000
+  }
+]
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results - cars' });
+  res.render('cars', { title: 'Search Results - cars', results: results });
 });
 
 module.exports = router;
