760301cbfa7e0a59d71c2b39c613ea9d25a05cfd
CYECHARLA <142927564+CYECHARLA@users.noreply.github.com>
2023-11-01 15:49:51 -0500

    added grid and choose

--- routes/board.js
+++ routes/board.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('board', { title: 'Board Display' });
-});
-
-// Inside the GET router for "board"
-router.get('/', (req, res) => {
+/* GET board page. */
+router.get('/', function (req, res, next) {
   let query = req.query;
   console.log(`rows: ${query.rows}`);
   console.log(`cols: ${query.cols}`);
-  res.render('board', { title: 'Board Display', query });
+  res.render('board', { title: 'Board Display', query: query });
 });
-
 
 module.exports = router;
