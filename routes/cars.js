1cecb1a5744ca46951d2d971642d182b400b17b8
CYECHARLA <142927564+CYECHARLA@users.noreply.github.com>
2023-11-01 17:52:57 -0500

    added onclick to buy

--- routes/cars.js
+++ routes/cars.js
@@ -25,4 +25,6 @@ router.get('/', function(req, res, next) {
   res.render('cars', { title: 'Search Results - cars', results: results });
 });
 
+
+
 module.exports = router;
--- routes/choose.js
+++ routes/choose.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path'); 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('choose', { title: 'Choose' });
+
+router.use(express.static(path.join(__dirname, 'C:\Users\S565903\projects\f23wb03yecharla\public\images')));
+
+router.get('/', (req, res) => {
+  const image_names = [
+    'car1.jpg',
+    'car2.jpg',
+    'car3.jpg',
+    'car4.jpg',
+    'car5.jpg'
+  ];
+
+  const selectedImages = [];
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * image_names.length);
+    const selectedImage = image_names[randomIndex];
+    selectedImages.push(selectedImage);
+  }
+
+  res.render('choose', { title: 'Choose', images: selectedImages });
 });
 
 module.exports = router;
+
+
+
